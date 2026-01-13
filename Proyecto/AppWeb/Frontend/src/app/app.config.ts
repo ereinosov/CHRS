c97a891657b9ea64882073a73a8712807257a1e2
Johnst2000 <jsilvat2@uteq.edu.ec>
2026-01-12 23:16:55 -0500

    Delete Proyecto/AppWeb directory

--- Proyecto/AppWeb/Frontend/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes'; // Importamos las rutas que definimos arriba
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes) // Aquí le inyectamos las rutas a la aplicación
-  ]
-};
